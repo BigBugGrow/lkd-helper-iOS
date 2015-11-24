faba03de78d51be7647c0aed6c85d3712042af46
BigBugGrow <bigbuggrow@gmail.com>
2015-11-24 19:44:39 +0800

    Initial Commit

---
+++ 辽科大助手/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  辽科大助手
+//
+//  Created by DongAn on 15/11/24.
+//  Copyright © 2015年 DongAn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 辽科大助手/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  辽科大助手
+//
+//  Created by DongAn on 15/11/24.
+//  Copyright © 2015年 DongAn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
